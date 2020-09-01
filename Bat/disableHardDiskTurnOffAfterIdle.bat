688e0a18117fc64d49d2de118aa759c484ed4226
Bfhrc7 <30432698+bouckdarko@users.noreply.github.com>
2020-09-01 14:02:57 +0200

    Create disableHardDiskTurnOffAfterIdle.bat

---
+++ Bat/disableHardDiskTurnOffAfterIdle.bat
@@ -0,0 +1,5 @@
+powercfg /SETDCVALUEINDEX SCHEME_CURRENT 0012ee47-9041-4b5d-9b77-535fba8b1442 6738e2c4-e8a5-4a42-b16a-e040e769756e 0
+powercfg /SETACVALUEINDEX SCHEME_CURRENT 0012ee47-9041-4b5d-9b77-535fba8b1442 6738e2c4-e8a5-4a42-b16a-e040e769756e 0
+
+Echo "Timer until hard drive turn off (when idle) changed to : never
+
