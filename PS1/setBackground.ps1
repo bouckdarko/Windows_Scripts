659ee4bdb3d737c2fcca1c713602b7c51c930013
Bfhrc7 <30432698+bouckdarko@users.noreply.github.com>
2020-09-01 13:51:58 +0200

    Create setBackground.ps1

---
+++ PS1/setBackground.ps1
@@ -0,0 +1,1 @@
+ Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value 'C:\Windows\System32\oobe\info\backgrounds\backgroundDefault.jpg' rundll32.exe user32.dll, UpdatePerUserSystemParameters
